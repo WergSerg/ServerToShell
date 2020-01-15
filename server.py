b9a77fcb417ac9f7b94c7a1f4244b95b32a533de
sergey.matsko <wwwser181@outlook.com>
2020-01-15 23:36:36 +0300

    commit 1

---
+++ server.py
@@ -0,0 +1,62 @@
+from flask import Flask, jsonify, abort, make_response, url_for, request
+from collections import namedtuple
+import smtplib
+import time
+import subprocess
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
+
+# config = configparser.ConfigParser()
+# config.read("settings.ini")
+# server_log = config.get("Settings", "Login")
+# server_pass = config.get("Settings", "pass")
+
+
+
+app = Flask(__name__)
+
+Login = ''
+role = ''
+
+def bat_():
+    filepath=r"C:\Users\sergey\Desktop\bat.bat"
+    print('start')
+    p = subprocess.Popen(filepath, shell=True, stdout = subprocess.PIPE)
+    print('finish')
+    stdout, stderr = p.communicate()
+    print(p.returncode) # is 0 if success
+
+@app.route('/start/all', methods=['POST'])
+def login_():
+    '''curl -i -H "Content-Type: application/json" -X POST -d "{\"State\": \"start\"}" http://127.0.0.1:5000'''
+    if not request.json or not 'State' in request.json:
+        abort(400)
+    State = request.json.get('State')
+    result ='true'
+    if root_id == 404:
+        return jsonify({'messg': 'oopss'}), 201
+    else:
+        return jsonify({'State': State, 'result': result}), 201
+
+
+@app.route('/startALL', methods=['GET'])
+def get_all_room():
+    roomst = namedtuple('roomst',['name','adress','chairs'])
+    room=roomst(1,2,3)
+    print(room.name)
+    rooms = []
+    bat_()
+    rooms.append({
+        'name': room.name,
+        'adress': room.adress,
+        'chairs': room.chairs,
+    })
+    return jsonify({'room': rooms})
+
+
+
+if __name__ == '__main__':
+    # app.run(debug=True)
+    app.run()
